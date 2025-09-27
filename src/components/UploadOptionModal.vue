594777fda23678310816bc3d4bcf469bd1e7a542
rc-devs <rc.devs9@gmail.com>
2025-09-27 10:11:40 -0500

    multiple button component from quasar

--- src/components/UploadOptionModal.vue
+++ src/components/UploadOptionModal.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>Upload Option component</div>
+  <div class="q-pa-md">
+    <q-btn-group spread>
+      <q-btn color="primary" label="Photo" icon="photo" />
+      <q-btn color="primary" label="File" icon="upload" />
+    </q-btn-group>
+  </div>
 </template>
 
 <script setup lang="ts">
