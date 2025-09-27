a4269af1b932b9cad45d88ac3c9a1cc7e363e7ed
rc-devs <rc.devs9@gmail.com>
2025-09-27 11:09:52 -0500

    utilize quasar file upload template

--- src/components/UploadFileModal.vue
+++ src/components/UploadFileModal.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>My component</div>
+  <div class="q-pa-md">
+    <div class="q-gutter-sm row items-start">
+      <q-uploader
+        url="http://localhost:4444/upload"
+        style="width: 300px"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
