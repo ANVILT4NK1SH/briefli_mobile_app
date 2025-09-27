d22848b221b2fe23b14d4eddc698b576e79e6fe0
rc-devs <rc.devs9@gmail.com>
2025-09-27 13:22:05 -0500

    utilize quasar upload component (image restricted)

--- src/components/UploadPhotoModal.vue
+++ src/components/UploadPhotoModal.vue
@@ -1,7 +1,39 @@
 <template>
-  <div>My component</div>
+  <div class="q-pa-md">
+    <div class="q-gutter-md row items-start">
+      <!-- what other types in 'accept'? -->
+      <q-uploader
+        style="max-width: 300px"
+        url="http://localhost:4444/upload"
+        label="Restricted to images"
+        multiple
+        accept=".jpg, .jpeg, .png, .gif, image/*"
+        @rejected="onRejected"
+      />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
-//
+<script lang="ts">
+import { useQuasar} from 'quasar'
+import type { QRejectedEntry } from 'quasar'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PhotoUploader',
+  setup() {
+    const $q = useQuasar()
+
+    const onRejected = (rejectedEntries: QRejectedEntry[]): void => {
+      $q.notify({
+        type: 'negative',
+        message: `${rejectedEntries.length} file(s) did not pass validation constraints`
+      })
+    }
+
+    return {
+      onRejected
+    }
+  }
+})
 </script>
