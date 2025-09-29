c8d6e92fd182ba491ecc2f97fbf655622c9ed000
rc-devs <rc.devs9@gmail.com>
2025-09-28 19:18:53 -0500

    remove 'placeholder' code, icon now account_circle

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,14 +7,12 @@
           <p style="font-size: 10px;"> Mobile Companion </p>
         </q-toolbar-title>
         <router-link to="/userPage" class="q-pa-md q-gutter-sm row justify-end items-center">
-          <!-- icon needs to reflect user/client logo, or some other icon -->
             <q-avatar
               size="xl"
               color="secondary"
               text-color="white"
-            >
-            <!-- {{ userInitial }} -->
-            </q-avatar>
+              icon="account_circle"
+            />
         </router-link>
       </q-toolbar>
     </q-header>
@@ -26,13 +24,5 @@
 </template>
 
 <script setup lang="ts">
-// placeholder script;
-/* import { userInfo } from 'os';
-import {computed, ref} from 'vue'
-
-const user = ref(userInfo)
-
-//get first initial from user.name (if it exists)
-const userInitial = computed(() => {
-return user.value.name?.charAt(0).toUpperCase() || ''}) */
+//
 </script>
