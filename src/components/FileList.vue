3a2afc5a0825f8863742df494814cb024cab2ba9
rc-devs <rc.devs9@gmail.com>
2025-09-29 20:11:56 -0500

    get bearer token onMounted/async

---
+++ src/components/FileList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>FileList</div>
+</template>
+
+<script setup lang="ts">
+import { useAuth0 } from '@auth0/auth0-vue';
+import { onMounted } from 'vue';
+
+//fetch bearer token for API calls
+const auth0 = useAuth0();
+let bearerToken: string | null = null;
+
+onMounted(async () => {
+  bearerToken = await auth0.getAccessTokenSilently();
+  console.log(bearerToken);
+});
+</script>
