575f9c7ca6c1a07c7cc471a1363771577b342f77
rc-devs <rc.devs9@gmail.com>
2025-10-02 18:02:19 -0500

    axios call, GET client list

--- src/components/ClientList.vue
+++ src/components/ClientList.vue
@@ -3,5 +3,25 @@
 </template>
 
 <script setup lang="ts">
-//
+import { useAuth0 } from '@auth0/auth0-vue';
+import axios from 'axios';
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+  //fetch bearer token for API calls
+  const auth0 = useAuth0();
+
+  const bearerToken = await auth0.getAccessTokenSilently();
+
+  axios
+    .get('https://demo-api.project-onyx-test.com/org/clients', {
+      headers: { Authorization: `Bearer ${bearerToken}` },
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+});
 </script>
