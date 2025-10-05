44b6ce0006960cd1639766e50f5ab507b8eb5575
rc-devs <rc.devs9@gmail.com>
2025-10-05 17:09:44 -0500

    filter files needing review utilizing fileService

--- src/components/ReviewFiles.vue
+++ src/components/ReviewFiles.vue
@@ -3,5 +3,11 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import FileList from './FileList.vue';
+import { filterByStatus } from 'src/services/fileService';
+
+onMounted(() => {
+  filterByStatus.value = ['PROCESSED', 'INVALID', 'ERROR'];
+}); //when mounted, invoke fileService and pass filter values
 </script>
