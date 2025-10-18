b4aa7b9bf892c8c96332782ccda2cdb3c2acda47
rc-devs <rc.devs9@gmail.com>
2025-10-18 18:05:56 -0500

    add condition for filtering by 'unassigned'/null id

--- src/services/fileService.ts
+++ src/services/fileService.ts
@@ -62,9 +62,16 @@ export const getFileCategoryStatus = (status: string | undefined) => {
 
 export const filterFilesByClientId = async (clientName: string | null) => {
   await getAllFiles(); // needed for when user filters then filters again (reset files)
+
   const clientId = getClientId(clientName); //convert client name to id
 
-  const filteredFilesByClientId = files.value.filter((file) => file.clientId === clientId); //filter files by id
-  files.value = filteredFilesByClientId; //set files to filtered array (ref updates ui)
-  return files.value;
+  if (clientName === 'Unassigned') {
+    const filteredFilesByClientId = files.value.filter((file) => file.clientId === null); //filter files by NO id
+    files.value = filteredFilesByClientId; //set files to filtered array (ref updates ui)
+    return files.value;
+  } else {
+    const filteredFilesByClientId = files.value.filter((file) => file.clientId === clientId); //filter files by id
+    files.value = filteredFilesByClientId; //set files to filtered array (ref updates ui)
+    return files.value;
+  }
 };
