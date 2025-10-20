42ff0bc29af3c6ba60279afd9240f55e207e4e63
rc-devs <rc.devs9@gmail.com>
2025-10-20 09:48:21 -0500

    create action getFilesFromApi to fill files[] in store

--- src/stores/FileStore.ts
+++ src/stores/FileStore.ts
@@ -1,7 +1,13 @@
 import { defineStore } from 'pinia';
+import { apiService } from 'src/services/apiService';
 
 export const useFileStore = defineStore('FileStore', {
-  state: () => ({ files: [] }),
+  state: () => ({ files: [] }), //returns state
   getters: {},
-  actions: {},
+  actions: {
+    //api call
+    async getFilesFromApi() {
+      this.files = (await apiService.getFiles()).data; //get files data and assign to files[] in state
+    },
+  },
 });
