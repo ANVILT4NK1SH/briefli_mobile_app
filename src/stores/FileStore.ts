710db7a33a847ecef6d1d08100a95d1733fadeee
rc-devs <rc.devs9@gmail.com>
2025-10-20 12:44:12 -0500

    get document by fileName and assign URLstring to pdfBlob state

--- src/stores/FileStore.ts
+++ src/stores/FileStore.ts
@@ -3,12 +3,20 @@ import type { ImportedDocument } from 'src/components/models';
 import { apiService } from 'src/services/apiService';
 
 export const useFileStore = defineStore('FileStore', {
-  state: () => ({ files: [] as ImportedDocument[] }), //returns state
-  getters: {},
+  state: () => ({ files: [] as ImportedDocument[], pdfBlob: '' }), //returns state
+  getters: {
+    //filter by status
+    //get color by type
+    //time elapsed
+    //abbreviate
+  },
   actions: {
-    //api call
+    //api calls
     async getFilesFromApi() {
       this.files = (await apiService.getFiles()).data; //get files data and assign to files[] in state
+    },
+    async getDocumentByFileName(filename: string, rotations: number[]) {
+      this.pdfBlob = await apiService.getDocument(filename, rotations); //returns url string
     },
   },
 });
