1ca7e9216dff3c83d61449db1295f753ff23d1ae
rc-devs <rc.devs9@gmail.com>
2025-10-20 17:28:54 -0500

    filter by client id in store

--- src/stores/FileStore.ts
+++ src/stores/FileStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia';
 import type { ImportedDocument } from 'src/components/models';
 import { apiService } from 'src/services/apiService';
+import { getClientId } from 'src/services/clientService';
 
 export const useFileStore = defineStore('FileStore', {
   state: () => ({ files: [] as ImportedDocument[], pdfBlob: '' }), //returns state
@@ -17,6 +18,17 @@ export const useFileStore = defineStore('FileStore', {
     },
     async getDocumentByFileName(filename: string, rotations: number[]) {
       this.pdfBlob = await apiService.getDocument(filename, rotations); //returns url string
+    },
+    async filterByCLientId(clientName: string) {
+      await this.getFilesFromApi(); // needed for when user filters then filters again (reset files)
+      const clientId = getClientId(clientName); //convert client name to id
+
+      if (clientName === 'Unassigned') {
+        this.files = this.files.filter((file) => file.clientId === null); //filter files by NO id
+      } else {
+        this.files = this.files.filter((file) => file.clientId === clientId); //filter files by id
+      }
+      return this.files;
     },
   },
 });
