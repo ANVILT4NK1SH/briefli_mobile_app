f3a593e1434d25b2bd8ceefad2c9597f11870382
rc-devs <rc.devs9@gmail.com>
2025-10-22 09:15:10 -0500

    set state values, getFiles getter, initialize status all

--- src/stores/FileStore.ts
+++ src/stores/FileStore.ts
@@ -2,12 +2,50 @@ import { defineStore } from 'pinia';
 import type { ImportedDocument } from 'src/components/models';
 import { apiService } from 'src/services/apiService';
 import { getClientId } from 'src/services/clientService';
+import { ref } from 'vue';
 
 export const useFileStore = defineStore('FileStore', {
-  state: () => ({ files: [] as ImportedDocument[], pdfBlob: '' }), //returns state
-  getters: {},
+  state: () => ({
+    files: ref<ImportedDocument[]>([]),
+    pdfUrl: ref(''),
+    statusBeingFiltered: ref<string[]>([]),
+    clientUnassigned: ref(false), //probably better naming convention?
+
+    //status categories (do these go here? not 'state'?)
+    statusOk: ref<string[]>(['APPROVED', 'EXPORTED', 'ARCHIVED', 'ARCHIVE_FAILED', 'EXPORTING']),
+    statusError: ref<string[]>(['REJECTED', 'FAILED', 'FAILURE', 'ERROR', 'INVALID']),
+    statusReviewNeeded: ref<string[]>(['PROCESSED', 'REPROCESSING', 'EXPORT_FAILED']),
+    statusAll: ref<string[]>([]),
+  }),
+  getters: {
+    //filter by status?
+    /*  filteredFiles: (state) => {
+      let result = state.files;
+
+      // Filter by client unassigned
+      if (state.clientUnassigned) {
+        result = result.filter((file) => !file.clientId);
+      }
+
+      // Filter by status, only if any status is selected
+      if (state.statusBeingFiltered.length > 0) {
+        result = result.filter((file) => state.statusBeingFiltered.includes(file.status));
+      }
+
+      return result;
+    }, */
+
+    getFiles: (state) => {
+      return state.files;
+    },
+  },
   actions: {
-    //api calls
+    //spread all statuses into statusAll
+    initializeStatusAll() {
+      this.statusAll = [...this.statusOk, ...this.statusError, ...this.statusReviewNeeded];
+    },
+
+    //api call for all files
     async getFilesFromApi() {
       this.files = (await apiService.getFiles()).data; //get files data and assign to files[] in state
     },
