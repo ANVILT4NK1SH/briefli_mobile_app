0c9534cbefdf956ccfb57c779400e8109ae4c702
rc-devs <rc.devs9@gmail.com>
2025-10-22 09:16:20 -0500

    remove unused refactored code blocks

--- src/services/fileService.ts
+++ src/services/fileService.ts
@@ -1,34 +1,6 @@
-import type { ImportedDocument } from 'src/components/models';
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
 
-export const files = ref<ImportedDocument[]>([]);
-
-// utilize these refs to set statuses across application (place status string inside array, and import variable where needed)
-export const statusOk = ref<string[]>([
-  'APPROVED',
-  'EXPORTED',
-  'ARCHIVED',
-  'ARCHIVE_FAILED',
-  'EXPORTING',
-]);
-export const statusError = ref<string[]>(['REJECTED', 'FAILED', 'FAILURE', 'ERROR', 'INVALID']);
-export const statusReviewNeeded = ref<string[]>(['PROCESSED', 'REPROCESSING', 'EXPORT_FAILED']);
-export const statusAll = ref<string[]>(
-  statusOk.value.concat(statusError.value.concat(statusReviewNeeded.value)),
-);
 export const clientUnassigned = ref(false);
-export const filterByStatus = ref<string[]>(statusAll.value);
-
-export const filteredFiles = computed(() => {
-  if (!filterByStatus.value || filterByStatus.value.length === 0) {
-    //if no filter or if array is empty, show all files
-    return files.value;
-  }
-  if (clientUnassigned.value === true) {
-    return files.value.filter((file) => !file.clientId);
-  }
-  return files.value.filter((file) => filterByStatus.value?.includes(file.status)); //filter using value/s from click to set condition
-});
 
 export const isInProcessedQueue = (status: string | undefined) => {
   if (!status) return false;
